aa71dcf4de0f6b0d64ba1b6403d65c31b2cd4360
apakestore <147926355+apakestore@users.noreply.github.com>
2023-10-14 20:10:53 +0700

    Update

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ global.APIs = {
 
 // Apikey
 global.APIKeys = {
-	'https://api.zeeoneofc.my.id': 'Your Key',
+	'https://api.zeeoneofc.my.id': '4ljUSxXm',
 }
 
 global.namabot = "Bot Store"
@@ -18,7 +18,7 @@ global.myweb = "https://s.id/zeeoneofc"
 global.footer_text = "© " + namabot
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
+global.owner = ['6285211527292']
 global.pairingNumber = "" //pairing number to install bot 
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
